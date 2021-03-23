c3564f91d52c8b6d07c6d6ae0d460da2e82ba06f
Haley Warson <haleywarson@gmail.com>
2021-03-23 09:31:54 -0600

    code complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const toyURL = "http://localhost:3000/toys/"
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,4 +13,84 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetchToys()
 });
+// -----------
+function fetchToys() {
+  fetch(toyURL)
+  .then(response => response.json())
+  .then (toys => toys.forEach(toy => renderCard(toy)))
+}
+
+document.querySelector('.add-toy-form').addEventListener
+("submit", (event) => {
+  event.preventDefault();
+  let newToy = {
+    "name": event.target.name.value,
+    "image": event.target.image.value,
+    "likes": 0
+  }
+  postToy(newToy)
+});
+
+function postToy(newToy) {
+  let options = {
+    method: "POST",
+    headers: 
+    {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(newToy)
+  }
+  fetch(toyURL, options)
+  .then(response => response.json())
+  .then(renderCard)   
+}
+
+function addLikes(toy, likes) {
+  let likesCount = parseInt(likes.innerText) + 1
+  let newLikes = {
+    "likes": likesCount
+  }
+
+  let options = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(newLikes)
+  }
+
+  fetch(toyURL + toy.id, options)
+  .then(response => response.json())
+  .then(updatedToy => likes.innerText = `${updatedToy.likes} Likes`)
+}
+
+
+function renderCard(toy) {
+  const toyCollection = document.querySelector('#toy-collection')
+  
+  const toyCard = document.createElement('div');
+  toyCard.setAttribute('class', 'card');
+
+  const toyName = document.createElement('h2');
+  toyName.textContent = toy.name;
+
+  const toyImage = document.createElement('img');
+  toyImage.src = toy.image;
+  toyImage.setAttribute('class', 'toy-avatar');
+
+  const likes = document.createElement('p');
+  likes.textContent = `${toy.likes} Likes`
+
+  const likeButton = document.createElement('button');
+  likeButton.setAttribute('class', 'like-btn');
+  likeButton.textContent = "Like";
+  likeButton.addEventListener("click", () => { addLikes(toy, likes) })
+
+  toyCard.append(toyName, toyImage, likes, likeButton)
+  toyCollection.append(toyCard)
+}
